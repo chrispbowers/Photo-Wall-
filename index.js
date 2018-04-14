71531166a3ada916fcc4b79e2902ecd7c28a1bec
Charlieclarke261097 <32364954+Charlieclarke261097@users.noreply.github.com>
2018-04-14 09:15:40 +0100

    1
    1

--- index.js
+++ index.js
@@ -24,41 +24,42 @@ function onPhotoDataSuccess(imageData) {
 	var image = document.getElementById('myImage');
 	myImage.style.display = 'block';
 	myImage.src = "data:image/jpeg;base64," + imageData;
+    onAddPhoto();
 }
 
 function onFail(message) {
       alert('Failed because: ' + message);
 }
 
-Backendless.Data.of("PHOTOS").find().then(processResults).catch(error);
+
 function processResults(photos){
     
-//add each photo
-for (var i = 0; i < photos.length; i++) {
- $("#images").append(photos[i].Image);
-}
+
     
 //refresh the table
-$("#images").content('refresh');
+$("#myImage").content('refresh');
 }
  function error(err){
-     alert(err); 
+     console.log("error1 " + err); 
  }
 
-$(document).on(onPhotoDataSuccess, onAddPhoto);
+
 
 //Add photo to backendless table
 function onAddPhoto() {
 console.log("Photo has been added");
+
+var byteArray = new Blob(["data:image/jpg;base64," + imageData]);
+ 
+Backendless.Files.saveFile( "Images" , "testimage.jpg", byteArray, true )
+ .then( function( savedFileURL ) {
+    console.log( "file has been saved - " + savedFileURL );
+  })
+ .catch( function( error ) {
+    console.log( "error2 - " + error.message );
+  }); 
     
-var photoimg = $("#images").val();    
-    
-var newPhoto = imageData ;
-newPhoto.photos = photoimg;
-Backendless.Data.of("Photos").save(newPhoto).then(saved).catch(error);
+
  
 }
     
-function saved(savedPhoto) {
-console.log( "new Contact instance has been saved" + savedPhoto);
-}
