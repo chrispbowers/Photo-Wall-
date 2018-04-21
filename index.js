08ea7f12939808199b3dd6ecaec8c3fc102114f5
Charlieclarke261097 <32364954+Charlieclarke261097@users.noreply.github.com>
2018-04-21 11:03:40 +0100

    1
    1

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function gotFile(fileObject){
     alert("gotFile " + fileObject);
 	
 	
-Backendless.Files.upload(fileObject, "My-folder", true)
+Backendless.Files.upload(fileObject, Images, true)
  .then( function( fileURL ) {
   })
  .catch( function( error ) {
