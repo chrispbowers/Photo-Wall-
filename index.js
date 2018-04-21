3c0fc3f7d524a10f8a1c994c2aaab1137c326962
Charlieclarke261097 <32364954+Charlieclarke261097@users.noreply.github.com>
2018-04-21 11:16:48 +0100

    3
    3

--- index.js
+++ index.js
@@ -82,8 +82,8 @@ function gotFile(fileObject){
     alert("gotFile " + fileObject);
 	
 	
-Backendless.Files.upload(fileObject, "Images", true)
- .then( function( imageURI ) {
+Backendless.Files.upload(file, "Images", true)
+ .then( function( fileURL) {
   })
  .catch( function( error ) {
   });
